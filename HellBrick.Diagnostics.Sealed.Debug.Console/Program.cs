5ec5b6891ad3dfbe6a39702c183d54a9d346b163
HellBrick <hellbrick@outlook.com>
2018-03-07 15:31:05 +0300

    Initialized the project structure

---
+++ HellBrick.Diagnostics.Sealed.Debug.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace HellBrick.Diagnostics.Sealed.Debug.Console
+{
+	internal class Program
+	{
+		private static async Task Main( string[] args )
+		{
+		}
+	}
+}
